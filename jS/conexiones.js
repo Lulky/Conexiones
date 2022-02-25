0150ee8be6bcbdde76729988f56b8111a8e0cc63
Lulky <lukyluxiano.12@gmail.com>
2022-02-24 19:28:10 -0500

    Conexiones

---
+++ jS/conexiones.js
@@ -0,0 +1,36 @@
+
+var element = document.querySelector(".luciano")
+
+function change(){
+element.innerText = "Paul Walker";
+}
+
+
+
+var count = 2;
+var num = document.querySelector(".number2");
+
+function ace(elemento){
+    count--;
+    num.innerText = count;
+    var wis = elemento.parentElement.parentElement;
+    wis.remove();
+}
+
+var coun = 500;
+var nam = document.querySelector(".number500");
+
+function tapi(){
+    
+    coun++;
+    nam.innerText = coun + "+";
+}
+
+
+
+
+
+
+
+
+
